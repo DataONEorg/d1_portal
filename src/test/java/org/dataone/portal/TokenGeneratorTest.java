f1d6b9b18f64a0bd65922a88266161cc55b8d089
Matthew B <106352182+artntek@users.noreply.github.com>
2024-02-19 09:50:31 -0800

    removed sleep. removed unnecessary code to help clarity

--- src/test/java/org/dataone/portal/TokenGeneratorTest.java
+++ src/test/java/org/dataone/portal/TokenGeneratorTest.java
@@ -5,18 +5,19 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
 import java.security.cert.X509Certificate;
 import java.security.interfaces.RSAPublicKey;
+import java.text.ParseException;
 import java.util.Calendar;
 import java.util.Date;
 
+import com.nimbusds.jose.JOSEException;
 import org.dataone.client.auth.CertificateManager;
 import org.dataone.configuration.Settings;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.nimbusds.jose.JWSVerifier;
@@ -25,20 +26,8 @@ import com.nimbusds.jwt.SignedJWT;
 
 public class TokenGeneratorTest {
 
-    @BeforeClass
-    public static void setUp() throws FileNotFoundException {
-
-//        File certificateFile = CertificateManager.getInstance().locateDefaultCertificate();
-//        URL url = TokenGeneratorTest.class.getResource("unitTestSelfSignedCert.pem");
-//        String certificatePath = url.getPath();
-//        String keyPath = url.getPath();
-//        Settings.getConfiguration().setProperty("cn.server.publiccert.filename", certificatePath);
-//        Settings.getConfiguration().setProperty("cn.server.privatekey.filename", keyPath);
-
-    }
-
     @Test
-    public void testBasicCalendarInstanceAssumptions() throws InterruptedException {
+    public void testBasicCalendarInstanceAssumptions() {
         // make sure getInstance() doesn't mean it's a singleton
         Calendar x = Calendar.getInstance();
         Date now = x.getTime();
@@ -47,7 +36,6 @@ public class TokenGeneratorTest {
         if (x == y) {
             fail("Calendar instances are the same. Not expected");
         }
-        Thread.sleep(2000);
         Date later = x.getTime();
         if (now.getTime() != later.getTime()) {
             fail("Calendar.getTime() should return a constant value over time (a Date object)");
@@ -68,54 +56,40 @@ public class TokenGeneratorTest {
 
 
     @Test
-    public void testGetJWT() {
-        try {
+    public void testGetJWT()
+        throws IOException, ParseException, JOSEException, NoSuchAlgorithmException {
+        String userId = "test";
+        String fullName = "Jane Scientist";
+        String token = TokenGenerator.getInstance().getJWT(userId, fullName);
 
-            String userId = "test";
+        // To parse the JWS and verify it, e.g. on client-side
+        SignedJWT signedJWT = SignedJWT.parse(token);
 
-            String fullName = "Jane Scientist";
+        // verify
+        String certificateFileName =
+            Settings.getConfiguration().getString("cn.server.publiccert.filename");
+        RSAPublicKey publicKey = (RSAPublicKey) CertificateManager.getInstance()
+            .loadCertificateFromFile(certificateFileName).getPublicKey();
 
-            String token = TokenGenerator.getInstance().getJWT(userId, fullName);
+        JWSVerifier verifier = new RSASSAVerifier(publicKey);
+        assertTrue(signedJWT.verify(verifier));
 
-            // To parse the JWS and verify it, e.g. on client-side
-            SignedJWT signedJWT = SignedJWT.parse(token);
+        // make sure the secret is required for verification
+        KeyPairGenerator keyGenerator = KeyPairGenerator.getInstance("RSA");
+        keyGenerator.initialize(1024);
+        KeyPair kp = keyGenerator.genKeyPair();
+        RSAPublicKey otherKey = (RSAPublicKey) kp.getPublic();
 
-            // verify
-            String certificateFileName =
-                Settings.getConfiguration().getString("cn.server.publiccert.filename");
-            RSAPublicKey publicKey = (RSAPublicKey) CertificateManager.getInstance()
-                .loadCertificateFromFile(certificateFileName).getPublicKey();
+        JWSVerifier invalidVerifier = new RSASSAVerifier(otherKey);
+        assertFalse(signedJWT.verify(invalidVerifier));
 
-            JWSVerifier verifier = new RSASSAVerifier(publicKey);
-            assertTrue(signedJWT.verify(verifier));
+        // Retrieve the JWT claims
+        assertEquals(userId, signedJWT.getJWTClaimsSet().getClaim("userId"));
+        assertEquals(userId, signedJWT.getJWTClaimsSet().getClaim("sub"));
+        assertEquals(userId, signedJWT.getJWTClaimsSet().getSubject());
 
-            // make sure the secret is required for verification
-            KeyPairGenerator keyGenerator = KeyPairGenerator.getInstance("RSA");
-            keyGenerator.initialize(1024);
-            KeyPair kp = keyGenerator.genKeyPair();
-            RSAPublicKey otherKey = (RSAPublicKey) kp.getPublic();
-
-            JWSVerifier invalidVerifier = new RSASSAVerifier(otherKey);
-            assertFalse(signedJWT.verify(invalidVerifier));
-
-            // Retrieve the JWT claims
-            assertEquals(userId, signedJWT.getJWTClaimsSet().getClaim("userId"));
-            assertEquals(userId, signedJWT.getJWTClaimsSet().getClaim("sub"));
-            assertEquals(userId, signedJWT.getJWTClaimsSet().getSubject());
-
-            assertTrue(Calendar.getInstance().getTime()
-                           .before(signedJWT.getJWTClaimsSet().getExpirationTime()));
-
-            //System.out.println("sub=" + signedJWT.getJWTClaimsSet().getClaim("sub"));
-            //System.out.println("iat=" + signedJWT.getJWTClaimsSet().getClaim("iat"));
-            //System.out.println("exp=" + signedJWT.getJWTClaimsSet().getClaim("exp"));
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            fail(e.getMessage());
-        }
-
+        assertTrue(Calendar.getInstance().getTime()
+                       .before(signedJWT.getJWTClaimsSet().getExpirationTime()));
     }
 
 }
